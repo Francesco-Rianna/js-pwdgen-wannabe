f6e3bb083d01c9a11749e336fcbd849ed33b9f71
Francesco-Rianna <maradonino98@hotmail.it>
2024-03-04 17:05:11 +0100

    feat: added bonus

--- script.js
+++ script.js
@@ -9,8 +9,22 @@ const userSurname = prompt('Ciao dimmi il tuo cognome');
 
 const userColor = prompt('Ciao dimmi il colore preferito');
 
+// BONUS
+
+// Chiediamo all'utente un numero
+
+const userNumber = prompt('Dimmi il tuo numero');
+console.log(userNumber);
+
+// Converto la stringa in un numero
+
+let userNumberAsNumeric = parseInt(userNumber);
+// Somma
+let sum = userNumberAsNumeric + 23;
+console.log(sum)
+
 // Scritta che esce dopo i prompt 
 
-let userCharacter = ` ${userName} ${userSurname} ${userColor} 23  `
+let userCharacter = ` ${userName} ${userSurname} ${userColor} ${sum}   `
 
 document.getElementById('message').innerHTML = userCharacter
